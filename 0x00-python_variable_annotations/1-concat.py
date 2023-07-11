b04a347d7a19eabdcc598fa220d3e53d6b754c73
Victor Emmanuel <victor.j.emmanuel@hotmail.com>
2023-07-11 13:00:04 +0000

    Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Contains a function that concatenates 2 strings."""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concatenates 2 strings."""
+    return str1 + str2
