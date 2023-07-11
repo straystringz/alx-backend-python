46a8c399dfccad113832654bfa3cfadb0aaabd55
Victor Emmanuel <victor.j.emmanuel@hotmail.com>
2023-07-11 13:03:58 +0000

    Basic annotations - to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Contains a single function that returns a string representation of a floating point"""
+
+
+def to_str(n: float) -> str:
+    """Returns a string representation of a float
+    Args:
+        n: float to be converted.
+    Returns:
+        string representation of the float
+    """
+    return str(n)
