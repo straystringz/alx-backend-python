a72ff46a5200fa693469735df806d7fb6f4be349
Victor Emmanuel <victor.j.emmanuel@hotmail.com>
2023-07-11 13:01:45 +0000

    Basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Contains a  function that calcs the floor of a number."""
+import math
+
+
+def floor(n: float) -> int:
+    """Returns the floor of a floating point."""
+    return math.floor(n)
